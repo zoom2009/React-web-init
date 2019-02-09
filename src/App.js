a4c5e14cca99a09c5b73c3db3f35da0d68b879b9
Sikarin Poonsawat <afumagic3@gmail.com>
2019-02-09 17:47:03 +0700

    add sass

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
+import './App.scss'
 
 export default class App extends Component {
   render() {
+    let status = 'active' 
+    // status = 'in-active'
     return (
-      <div>
+      <div id="main-app">
         <p>Hello World, Good luck.</p>
           <p>This project have</p>
           <p>=> HOOK</p>
           <p>=> Mobx with Decorators</p>
           <p>=> .emmet</p>
+          <p>=> sass</p>
+
+          <button className={`mySwitch mySwitch-${status}`}>Button</button>
       </div>
     )
   }
